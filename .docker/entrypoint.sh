7b6903596e1f794aa9e4ed61a3dd64aa04e750a7
Israel Brito <israel.brito@agilize.com.br>
2020-04-11 18:49:27 -0300

    Ajusta entrypoint causador do erro, adicionado copia da .env

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+
+cp -R -u -p .env.development .env
+sleep 1
 composer install
 
-cp .env.example .env
 chown -R www-data: /var/www/
 chown -R 1000: /var/www/.docker/dbdata
 chmod 777 -R /var/www/
 
-sleep 2
-
-php artisan migrate &&
-    php artisan cache:clear &&
-    php artisan key:generate &&
-    php artisan config:cache
+php artisan migrate
+sleep 1
+php artisan cache:clear
+#php artisan key:generate
+sleep 1
+php artisan config:cache
 
 php-fpm
